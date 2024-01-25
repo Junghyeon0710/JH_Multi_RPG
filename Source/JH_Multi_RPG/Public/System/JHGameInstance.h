2c9e4c049b4f5057b1d21db0448d76c91b9a5d46
Junghyeon <sie08357@naver.com>
2024-01-25 23:40:35 +0900

    메인메뉴

---
+++ Source/JH_Multi_RPG/Public/System/JHGameInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "JHGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JH_MULTI_RPG_API UJHGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+
+};
