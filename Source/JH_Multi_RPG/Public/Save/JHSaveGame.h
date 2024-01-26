74b11ff32d665c5102b9b0467c1dfa4ea6024908
Junghyeon <sie08357@naver.com>
2024-01-26 23:05:29 +0900

    메인메뉴 캐릭터 선택 메뉴

---
+++ Source/JH_Multi_RPG/Public/Save/JHSaveGame.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "JHSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JH_MULTI_RPG_API UJHSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+};
